1b378423e2dadaf4d8018b62eadfbecbd0347c92
“Meg <meg5ed@virginia.edu>
2021-09-16 18:43:26 -0600

    added dummy data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+// dummy data
+  state = {
+    groceries = [
+      { id: 1, item: "Milk",  complete: false, price: 2.50},
+      { id: 2, item: "Broccoli", complete: false, price: 1.50},
+      { id: 3, item: "Beans", complete: true, price: 0.99},
+      { id: 4, item: "Rice", complete: false, price: 1.20},
+      { id: 5, item: "Beer", complete: false, price: 4.99}
+    ]
+  }
+
+  getUniqId = () => {
+    return Math.floor((1 + Math.random()) * 0x1000)
+    .toString(16)
+    .substring(1);
+  }
+ 
+  addToList = (incomingGroceryItem) => {
+    const { groceries } = this.state
+    const newGroceryItem = { id: this.getUniqId() , ...imcomingGroceryItem }
+    this.setState({groceries: [newGroceryItem,  ...groceries]})
+  }
+
+
+  render () {
+    const { groceries } = this.state
+    return  {
+      <>
+        // enter new JS components here
+      </>
+    }
+  }
 }
 
 export default App;
+
