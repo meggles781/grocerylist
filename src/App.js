2af9fea15acf2c355c465537df08e5c04aff63a6
“Meg <meg5ed@virginia.edu>
2021-09-16 20:51:40 -0600

    added styles

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+
+import "./App.css";
 import { Component } from 'react';
 import GroceryForm from './components/groceries/GroceryForm';
 import GroceryList from './components/groceries/GroceryList';
